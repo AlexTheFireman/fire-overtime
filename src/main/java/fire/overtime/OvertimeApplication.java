dec433bf324091723dc0e1dfb7be6ab9242ce7ec
AlexTheFireman <bizhov.alexander@gmail.com>
2021-08-01 16:22:22 +0300

    init

---
+++ src/main/java/fire/overtime/OvertimeApplication.java
@@ -0,0 +1,13 @@
+package fire.overtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fire/overtime/OvertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package fire.overtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
